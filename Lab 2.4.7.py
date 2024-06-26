31078b2fe8681967c59997cc800653f30a9dd8d8
pdil321 <g_p656@yahoo.com>
2024-06-26 07:53:03 +0100

    Update

---
+++ Lab 2.4.7.py
@@ -0,0 +1,11 @@
+John = 3
+Mary = 5
+Adam = 6
+total_apples = John + Mary + Adam
+
+print(John,Mary,Adam,sep=",")
+
+print(total_apples)
+print("Total apples = ",total_apples)
+print(total_apples//John)
+print(total_apples%Adam)
