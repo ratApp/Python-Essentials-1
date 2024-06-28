7fb4f9930a7ce3973a8a7e3401a2d9e41672a0f6
ratApp <80206064+ratApp@users.noreply.github.com>
2024-06-28 09:15:55 +0100

    Update Lab 2.6.11.py

--- Lab 2.6.11.py
+++ Lab 2.6.11.py
@@ -5,9 +5,9 @@ mins = int(input("Starting time (minutes): "))
 dura = int(input("Event duration (minutes): "))
 
 # Write your code here.
-start_time = hour * 60 + mins   # start time in minutes
+start_time = hour * 60 + mins   # Start time converted to minutes
 
-end_time = start_time + dura
-end_time_hr = end_time // 60
-end_time_min = end_time % 60
-print(end_time_hr, end_time_min, sep=":")
+end_time = start_time + dura    # Total end time in minutes
+end_time_hr = end_time // 60    # Output hours of the end time
+end_time_min = end_time % 60    # Output minutes of the end time
+print(end_time_hr, end_time_min, sep=":")     #Prints end time in HH:MM format
