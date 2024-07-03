6629a05ef1f023fc5bc18fe08aef9f0809e67e3f
ratApp <80206064+ratApp@users.noreply.github.com>
2024-07-03 09:41:14 +0100

    Update Lab 3.2.4.py

--- Lab 3.2.4.py
+++ Lab 3.2.4.py
@@ -17,8 +17,8 @@ print(  # Prints the welcome message
 number = int(input("Enter a number: "))  # Asks user to enter number to make a guess and assign to variable 'number'
 
 while number != secret_number:   # Untill the nunmber picked by the user matches the secret number
-  print("Ha ha! You're stuck in my loop")    # Informs user they are stuck in a loop
-  number = int(input("Try again!! Enter another number : "))    # Asks the user to try again
-  #continue    # Ends the current iteration and continue to the next loop
-else:     # If user guesses correctly
-  print("Well done, muggle! You are free now")    # Informs user they are correct and out of the continuous loop
+  print("Ha ha! You're stuck in my loop\n")    # Informs user they are stuck in a loop
+  number = int(input("Try again!! Enter a another number : "))    # Asks the user to try again
+
+# If user guesses correctly
+print("Well done, muggle! You are free now\n")    # Informs user they are correct and out of the continuous loop
