47f37f2ac78da7946ce84500ff3fa8f61d976301
ratApp <80206064+ratApp@users.noreply.github.com>
2024-07-09 12:39:39 +0100

    Update Lab 4.3.8 Fuel consumption converter.py

--- Lab 4.3.8 Fuel consumption converter.py
+++ Lab 4.3.8 Fuel consumption converter.py
@@ -17,19 +17,37 @@
 # 1 American gallon = 3.785411784 litres.
 
 def liters_100km_to_miles_gallon(liters):
-    #
-    # Write your code here.
-    #
+    meters = 100000
+    american_mile = meters / 1609.344
+    american_gallon = liters / 3.785411784
+
+    miles_gallon = american_mile / american_gallon
+    return miles_gallon
+
+
 
 def miles_gallon_to_liters_100km(miles):
-    #
-    # Write your code here.
-    #
+    american_gallon = 1
+    liters = american_gallon * 3.785411784
+    meters = miles * 1609.344
+    km_100 = meters /100000
+
+    liters_100km = liters / km_100
+    return liters_100km
+
+#Expected output:
+
+#60.31143162393162
+#31.36194444444444
+#23.52145833333333
+#3.9007393587617467
+#7.490910297239916
+#10.009131205673757
 
 print(liters_100km_to_miles_gallon(3.9))
 print(liters_100km_to_miles_gallon(7.5))
-print(liters_100km_to_miles_gallon(10.))
-print(miles_gallon_to_liters_100km(60.3))
-print(miles_gallon_to_liters_100km(31.4))
+print(liters_100km_to_miles_gallon(10.1))
 print(miles_gallon_to_liters_100km(23.5))
+print(miles_gallon_to_liters_100km(31.4))
+print(miles_gallon_to_liters_100km(60.3))
 
