3034ab483b19459e362059fb422cc27c9a97ca84
pdil321 <g_p656@yahoo.com>
2024-08-22 09:24:41 +0100

    modified

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 tupl = tuple('Python World!')
-print(tupl[:-7])
+print(tupl[:1])
+print(tupl[::-3])
 
 def fun(a=3,b=2):
   return a ** b
@@ -43,3 +44,25 @@ for i in range(1,4):
   break
 else:
   print("hi")
+
+my_list = [1, 2, 3, 4]
+print(my_list[-3:-2])
+
+my_list = [x * x for x in range(5)]
+
+print(my_list)
+
+lst = [i for i in range(-1, -2)]
+print(lst)
+
+dd = {"1": "0", "0": "1"}
+for x in dd.values():
+    print(x, end="")
+
+lst1 = [[x for x in range(3)] for y in range(3)]
+print(lst1)
+
+for r in range(3):
+    for c in range(3):
+        if lst1[r][c] % 2 != 0:
+            print("#")
